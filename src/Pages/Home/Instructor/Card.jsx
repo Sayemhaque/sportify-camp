d2d97c516f8a1aa05501610a9c0c6599a65bd880
sayemhaque <mdsayemofficial1000@gmail.com>
2023-06-11 13:58:21 +0600

    some animation added

--- src/Pages/Home/Instructor/Card.jsx
+++ src/Pages/Home/Instructor/Card.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
 import {useContext} from "react"
 import { ThemeContext } from "../../../Provider/ThemeProvider";
+import { Fade } from "react-awesome-reveal";
 
 const Card = ({instructor }) => {
     const {image,name,email} = instructor;
     const {isDarkMode} = useContext(ThemeContext)
     return (
+      <Fade triggerOnce>
         <div className={`card w-full  shadow-xl ${isDarkMode ? 'bg-slate-800 text-white' : 'bg-orange-300'}`}>
   <figure><img className="h-[400px] w-full object-cover" src={image} alt="Shoes" /></figure>
   <div className="card-body">
@@ -13,6 +15,7 @@ const Card = ({instructor }) => {
     <p>Email:{email}</p>
   </div>
 </div>
+</Fade>
     );
 };
 
--- src/Pages/Home/popular classes/Card.jsx
+++ src/Pages/Home/popular classes/Card.jsx
@@ -1,23 +1,26 @@
 /* eslint-disable react/prop-types */
-import {useContext} from "react"
+import { useContext } from "react"
 import { ThemeContext } from "../../../Provider/ThemeProvider";
+import { Fade } from "react-awesome-reveal";
 
 
-const Card = ({Class }) => {
-    const {image,className,instructor,seats,totalEnroll} = Class;
-    const {isDarkMode} = useContext(ThemeContext)
-    return (
-        <div className={`card w-full  shadow-xl ${isDarkMode ? 'bg-slate-800 text-white' : 'bg-orange-300'}`}>
-  <figure><img className="h-[300px] w-full object-cover" src={image} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title text-2xl ">{className}</h2>
-    <p className="font-semibold"><span className="font-bold">Instructor: </span>{instructor}</p>
-    <p className="font-semibold"><span className="font-bold">Available seats: </span>{seats}</p>
-    <p className="font-semibold"><span className="font-bold">Total Enroll:
-     </span>{totalEnroll}</p>
-  </div>
-</div>
-    );
+const Card = ({ Class }) => {
+  const { image, className, instructor, seats, totalEnroll } = Class;
+  const { isDarkMode } = useContext(ThemeContext)
+  return (
+    <Fade triggerOnce>
+      <div className={`card w-full  shadow-xl ${isDarkMode ? 'bg-slate-800 text-white' : 'bg-orange-300'}`}>
+        <figure><img className="h-[300px] w-full object-cover" src={image} alt="Shoes" /></figure>
+        <div className="card-body">
+          <h2 className="card-title text-2xl ">{className}</h2>
+          <p className="font-semibold"><span className="font-bold">Instructor: </span>{instructor}</p>
+          <p className="font-semibold"><span className="font-bold">Available seats: </span>{seats}</p>
+          <p className="font-semibold"><span className="font-bold">Total Enroll:
+          </span>{totalEnroll}</p>
+        </div>
+      </div>
+    </Fade>
+  );
 };
 
 export default Card;
