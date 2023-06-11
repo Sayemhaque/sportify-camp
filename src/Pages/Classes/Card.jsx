63ce8951f54bedf283c912f75bb911429e3f97ba
sayemhaque <mdsayemofficial1000@gmail.com>
2023-06-11 13:33:43 +0600

    react awsome reveal added

--- src/Pages/Classes/Card.jsx
+++ src/Pages/Classes/Card.jsx
@@ -7,6 +7,7 @@ import { FirebaseAuthContext } from "../../Provider/AuthProvider";
 import useAdmin from "../../hooks/useAdmin";
 import useInstructor from "../../hooks/useInstructor";
 import Swal from "sweetalert2";
+import { Fade } from "react-awesome-reveal";
 
 const Card = ({ Class }) => {
   const { user } = useContext(FirebaseAuthContext)
@@ -44,20 +45,24 @@ const Card = ({ Class }) => {
     }
   }
   return (
-<div className={`card w-full  shadow-xl ${isDarkMode ? 'bg-slate-800 text-white' : 'bg-orange-300'} ${seats < 1 && "bg-red-500 text-white"}`}>
-      <figure><img className="h-[300px] w-full object-cover" src={image} alt="Shoes" /></figure>
-      <div className="card-body">
-        <h2 className="card-title text-2xl">{className}</h2>
-        <p className="font-bold text-xl">{instructor}</p>
-        <p className="font-semibold">Available Seat:{seats}</p>
-        <p className="font-semibold ">Price:<span className="text-sm">{price}$</span></p>
-        <div className="mt-4">
-          <button className={`bg-white px-5 py-2 font-bold  rounded-md disabled:bg-opacity-60 disabled:cursor-not-allowed ${isDarkMode ? "text-black" : ""}`} disabled={!student || seats < 1} onClick={handleSelect}>
-            Select
-          </button>
-        </div>
+<Fade triggerOnce>
+  <div className={`card w-full shadow-xl ${isDarkMode ? 'bg-slate-800 text-white' : 'bg-orange-300'} ${seats < 1 && 'bg-red-500 text-white'}`}>
+    <figure>
+      <img className="h-[300px] w-full object-cover" src={image} alt="Shoes" />
+    </figure>
+    <div className="card-body">
+      <h2 className="card-title text-2xl">{className}</h2>
+      <p className="font-bold text-xl">{instructor}</p>
+      <p className="font-semibold">Available Seat: {seats}</p>
+      <p className="font-semibold">Price: <span className="text-sm">{price}$</span></p>
+      <div className="mt-4">
+        <button className={`bg-white px-5 py-2 font-bold rounded-md disabled:bg-opacity-60 disabled:cursor-not-allowed ${isDarkMode ? 'text-black' : ''}`} disabled={!student || seats < 1} onClick={handleSelect}>
+          Select
+        </button>
       </div>
     </div>
+  </div>
+</Fade>
   );
 };
 
