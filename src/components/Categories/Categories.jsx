5c86927458bab8bd80bac825161781a738d12e98
sayemhaque <mdsayemofficial1000@gmail.com>
2023-08-07 17:34:11 +0600

    ui changes

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -5,7 +5,7 @@ import soccer from "../../assets/soccer-player.png";
 import tennis from "../../assets/tennis-player-with-racket.png";
 const Categories = () => {
     return (
-        <div className="grid grid-cols-5 gap-5 place-items-center py-12">
+        <div className="grid grid-cols-2 md:grid-cols-5 gap-5 place-items-center py-12">
             <div className="bg-gray-200 p-6">
                <img className="w-[100px] hover:rotate-90 cursor-pointer duration-500" src={BaseBall} alt="" />
             </div>
